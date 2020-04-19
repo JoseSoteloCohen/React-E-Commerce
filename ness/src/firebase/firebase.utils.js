449175910a4d1307a8174b27e7a2196781f267f5
Jose Sotelo <jsotelocohen@gmail.com>
2020-04-19 14:38:13 -0500

    Sign in and auth logic
    Now we create the user in the database

--- ness/src/firebase/firebase.utils.js
+++ ness/src/firebase/firebase.utils.js
@@ -11,6 +11,31 @@ const config = {
     messagingSenderId: "955128727990",
     appId: "1:955128727990:web:48d21dec0171afd48f5917"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists){
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try{
+          await userRef.set({
+              displayName,
+              email,
+              createdAt,
+              ...additionalData
+          })
+      }catch (e) {
+          console.log('error creating user', e.message);
+      }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
