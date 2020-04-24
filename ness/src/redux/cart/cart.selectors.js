8ecced6a1875f45db8440c187580c622a9a41ef5
Jose Sotelo <jsotelocohen@gmail.com>
2020-04-24 17:06:35 -0500

    Checkout items

--- ness/src/redux/cart/cart.selectors.js
+++ ness/src/redux/cart/cart.selectors.js
@@ -1,4 +1,4 @@
-import {createSelector} from "reselect";
+import { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
 
@@ -10,11 +10,24 @@ export const selectCartItems = createSelector(
 export const selectCartHidden = createSelector(
     [selectCart],
     cart => cart.hidden
-)
+);
 
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems =>
-        cartItems.reduce((accumulatedQuantity, cartItem) =>
-            accumulatedQuantity + cartItem.quantity, 0)
-    );
+        cartItems.reduce(
+            (accumalatedQuantity, cartItem) =>
+                accumalatedQuantity + cartItem.quantity,
+            0
+        )
+);
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce(
+            (accumalatedQuantity, cartItem) =>
+                accumalatedQuantity + cartItem.quantity * cartItem.price,
+            0
+        )
+);
